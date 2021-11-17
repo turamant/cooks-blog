475b6e448e481af748845ff68cc6c6f819cce39f
askvart <tur1amant@gmail.com>
2021-11-17 16:07:04 +0300

    blog/urls.py/search.html

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.blog.views import detail, category
+from apps.blog.views import detail, category, search
 
 urlpatterns = [
+    path('search/', search, name='search'),
     path('<slug:category_slug>/<slug:slug>/', detail, name='post_detail'),
     path('<slug:slug>/', category, name='category_detail')
 ]
