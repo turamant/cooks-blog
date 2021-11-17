6a19934e98720887f51fe4d6144648d27df83682
askvart <tur1amant@gmail.com>
2021-11-17 13:58:24 +0300

    blog/models.py + class Comment

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -21,3 +21,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.blog.views import detail
 
 urlpatterns = [
-    path('<slug:slug>/', detail, name='detail')
+    path('<slug:slug>/', detail, name='post_detail')
 ]
