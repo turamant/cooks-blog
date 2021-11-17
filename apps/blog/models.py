dbe3495d63b316ff800f9f73845dd457498aa422
askvart <tur1amant@gmail.com>
2021-11-17 13:04:15 +0300

    blog/models.py class Post

---
+++ apps/blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Post(models.Model):
+    ACTIVE = 'active'
+    DRAFT = 'draft'
+    CHOICES_STATUS = (
+        (ACTIVE, 'Active'),
+        (DRAFT, 'Draft')
+    )
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
