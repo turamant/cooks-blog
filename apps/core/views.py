c3a83e4ed697425fc52020c98c5c7d3fa27c299d
askvart <tur1amant@gmail.com>
2021-11-17 16:29:41 +0300

    core/views.py def robots_txt()

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from apps.blog.models import Post
@@ -9,3 +10,11 @@ def frontpage(request):
 
 def about(request):
     return render(request, 'core/about.html')
+
+def robots_txt(request):
+    text = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+    ]
+    return HttpResponse("\n".join(text), content_type="text/plain")
+
