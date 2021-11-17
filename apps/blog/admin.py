9e6b311da016cac6fd4f3177a645ab1b91946785
askvart <tur1amant@gmail.com>
2021-11-17 13:17:56 +0300

    blog/urls.py

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from apps.blog.models import Post
 
-@admin.register
-class BlogAdmin(admin.ModelAdmin):
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'intro', 'created_at',
                     'status')
     prepopulated_fields = {'slug': ('title',)}
