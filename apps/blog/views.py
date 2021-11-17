d8bcdf669af923145794601bb916263be10613af
askvart <tur1amant@gmail.com>
2021-11-17 14:05:55 +0300

    blog/views.py + form

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 
+from apps.blog.forms import CommentForm
 from apps.blog.models import Post
 
 
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/detail.html', {'post': post})
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CommentForm()
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
 
