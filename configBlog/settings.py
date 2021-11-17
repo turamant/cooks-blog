cfa9c8f7ddc54e2895df7724d8564ea7034b2917
askvart <tur1amant@gmail.com>
2021-11-17 19:37:34 +0300

    heroku new

--- configBlog/settings.py
+++ configBlog/settings.py
@@ -129,7 +129,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'siteblog/static'),
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
