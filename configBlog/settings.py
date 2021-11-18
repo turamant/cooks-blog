0de96b9a3a55aa0040dfacfeef4b7a453fd17562
askvart <tur1amant@gmail.com>
2021-11-18 13:14:17 +0300

    push to github

--- configBlog/settings.py
+++ configBlog/settings.py
@@ -22,7 +22,7 @@ from .local_settings import *
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s%_2=hl6!5b0_6c1_mh5(iz%c47bux)b22^yoceheeih(f-y_!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
